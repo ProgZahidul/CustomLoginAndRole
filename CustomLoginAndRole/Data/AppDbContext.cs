e787922ab3c72afa405541f51db31150c79492e3
saikat hoque <sayedzahidulhoquesaikat@gmail.com>
2024-12-05 00:31:18 +0600

    Add project files.

---
+++ CustomLoginAndRole/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CustomLoginAndRole.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomLoginAndRole.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
